73ee594aca1d5984d71b16f752cb996f6c96ea2b
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-03-18 18:28:51 -0300

    ci: test

--- src/sub.test.js
+++ src/sub.test.js
@@ -1,9 +1,9 @@
 const { sub } = require("./sub");
 
 test("sub two numbers", () => {
-  // expect(sub(2, 1)).toBe(1);
+  expect(sub(2, 1)).toBe(1);
 });
 
-// test("sub two numbers, negative", () => {
-//   expect(sub(1, 30)).toBe(0);
-// });
+test("sub two numbers, negative", () => {
+  expect(sub(1, 30)).toBe(0);
+});
