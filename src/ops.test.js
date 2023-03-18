01cfaedf6beaf3cd4ce92e5ef46c32421b3844cd
Aryella Lacerda <aryella.lacerda@gmail.com>
2023-03-18 18:21:07 -0300

    ci: test

--- src/ops.test.js
+++ src/ops.test.js
@@ -4,6 +4,6 @@ test("add two numbers", () => {
   expect(add(1, 1)).toBe(2);
 });
 
-test("add two numbers, negative", () => {
-  expect(add(1, -30)).toBe(0);
-});
+// test("add two numbers, negative", () => {
+//   expect(add(1, -30)).toBe(0);
+// });
